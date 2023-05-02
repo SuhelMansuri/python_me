78f602f8561ba656f0ecaecb5025540053438f80
Suhel Mansuri <suhel.mansuri100@gmail.com>
2023-05-02 16:37:49 -0400

    added step_by_step Modules_Packages

---
+++ learn-python-3-step-by-step/Modules_Packages/mymodule.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self,roll,name):
+        self.roll = roll
+        self.name = name
+        
+    def print(self):
+        print(f'Name = {self.name}, roll = {self.roll}')
+        
+def add(first, second):
+    return first + second
+
---
+++ learn-python-3-step-by-step/Modules_Packages/test.py
@@ -0,0 +1,6 @@
+from mymodule import *
+
+s = Student(1, 'John')
+s.print()
+r = add(10,20)
+print(f'Result of additon is: {r} ')
